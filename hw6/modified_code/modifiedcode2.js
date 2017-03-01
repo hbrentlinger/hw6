40e0c6f89120a07aae14d888fb9375a0d7c05117
hbrentlinger <hbrentlinger@cca.edu>
2017-03-01 09:03:02 -0800

    Add files via upload

--- hw6/modified_code/modifiedcode2.js
+++ hw6/modified_code/modifiedcode2.js
@@ -1,5 +1,5 @@
-var shade = [50, 20, 45, 25, 60,80,30,12,77]
-
+var shade = [50, 20, 45, 25, 60, 80, 30, 12, 77]
+var x = 0;
 
 function setup() {
 	createCanvas(400, 400);
@@ -8,10 +8,15 @@ function setup() {
 function draw() {
 	colorMode(HSB);
 	noStroke();
-	
 	for (i = 0; i < shade.length; i++) {
-
-	fill(shade[i], 100, 100);
-	rect(i*50+100 / 5, 0, width / 5, height);
+		fill(shade[i], 100, 100);
+		var x = 0;
+		rect(i * width / shade.length + x, 0, width / shade.length, height);
+		shade[shade.length - 1] = mouseY;
+		x = x - .1;
+	}
 }
+
+function mouseClicked() {
+	append(shade, random(0, 360))
 }
